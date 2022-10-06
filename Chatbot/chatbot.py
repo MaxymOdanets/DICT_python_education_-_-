a4acc5d9233d0f3bf7ca4fe40f8e6917734b751c
Maxym Odanets <m.r.odanets@student.khai.edu>
2022-10-06 16:40:42 +0300

    Chatbot 3-rd stage

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -1,7 +1,11 @@
-print("Hello! My name is Garik")
-print("I was created in 2022")
+print("Hello! My name is Garik \nI was created in 2022")
 print("Please remind me your name: ")
 name = input(">")
 print(f"What a great name you have,{name}")
-
+print("Let me guess your age? \nEnter remainders of dividing your age by 3, 5 and 7")
+remainder3 =int(input(">"))
+remainder5 =int(input(">"))
+remainder7 =int(input(">"))
+age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+print("Your age is",age,"that's a good time to start programming!")
 
