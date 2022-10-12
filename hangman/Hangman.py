5547fbf96ba61eaecb9a9d22b91bbec2e4c40ec6
Maxym Odanets <m.r.odanets@student.khai.edu>
2022-10-12 19:37:44 +0300

    hangman 4-th stage

--- hangman/Hangman.py
+++ hangman/Hangman.py
@@ -2,13 +2,12 @@ import random
 from random import choice
 
 print("HANGMAN\nThe game will be available soon.")
-words = ["python" , "java" , "php" , "html"]
+words = ["python" , "java" , "javascript" , "html"]
 word = random.choice(words)
-a = input("Guess the word :\n>")
-while True:
-    if a == word:
-        print("You survived!")
-        break
-    else:
-        print("You lost!")
-        break
+length = '-' * len(word)
+a = input("Guess the word :" + word[:3] + "-".join([''for _ in range (len(word)-3)]) + "-\n>")
+if a == word:
+    print("You survive!")
+else:
+    print("You lose!")
+
