97d88ae22371c0c87b3269d53e48f73eaeb79000
Maxym Odanets <m.r.odanets@student.khai.edu>
2022-10-12 18:39:29 +0300

    hangman 2-nd stage

--- hangman/Hangman.py
+++ hangman/Hangman.py
@@ -1,1 +1,10 @@
 print("HANGMAN\nThe game will be available soon.")
+word = ["python"]
+a = input("Guess the word :\n>")
+while True:
+    if a == word:
+        print("You survived!")
+        break
+    else:
+        print("You lost!")
+        break
