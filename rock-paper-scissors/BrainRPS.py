f9bfd03801195e9bd1f598f4261a36340c98795a
Maxym Odanets <m.r.odanets@student.khai.edu>
2023-03-09 15:29:19 +0200

    RockPaperScissors 3-rd stage

--- rock-paper-scissors/BrainRPS.py
+++ rock-paper-scissors/BrainRPS.py
@@ -3,18 +3,27 @@ import random
 class BrainRockPaperScissors:
     @staticmethod
     def start():
+        user_list = ['rock', 'paper', 'scissors', '!exit']
         bot_options_list = ['rock', 'paper', 'scissors']
-        bot_options = random.choice(bot_options_list)
-        option = input("type your symbol\n>")
-        if bot_options == 'rock':
-            return BrainRockPaperScissors.bot_choose_rock(bot_options, option), bot_options
-        elif bot_options == 'paper':
-            return BrainRockPaperScissors.bot_choose_paper(bot_options, option), bot_options
-        elif bot_options == 'scissors':
-            return BrainRockPaperScissors.bot_choose_scissors(bot_options, option), bot_options
+        while True:
+            option = input("type your symbol\n>")
+            if option == '!exit':
+                print("exiting game...")
+                return -1, None
+            elif option not in user_list:
+                print("Invalid input, try again")
+                continue
+            bot_options = random.choice(bot_options_list)
+            if bot_options == 'rock':
+                return BrainRockPaperScissors.bot_choose_rock(bot_options, option), bot_options
+            elif bot_options == 'paper':
+                return BrainRockPaperScissors.bot_choose_paper(bot_options, option), bot_options
+            elif bot_options == 'scissors':
+                return BrainRockPaperScissors.bot_choose_scissors(bot_options, option), bot_options
 
     @staticmethod
     def bot_choose_rock(bot_options, option):
+        win = None
         if bot_options == option:
             win = 0
         elif bot_options == 'rock' and option == 'paper':
@@ -25,6 +34,7 @@ class BrainRockPaperScissors:
 
     @staticmethod
     def bot_choose_paper(bot_options, option):
+        win = None
         if bot_options == option:
             win = 0
         elif bot_options == 'paper' and option == 'scissors':
@@ -35,6 +45,7 @@ class BrainRockPaperScissors:
 
     @staticmethod
     def bot_choose_scissors(bot_options, option):
+        win = None
         if bot_options == option:
             win = 0
         elif bot_options == 'scissors' and option == 'rock':
--- rock-paper-scissors/rock_paper_scissors.py
+++ rock-paper-scissors/rock_paper_scissors.py
@@ -1,10 +1,13 @@
 from BrainRPS import BrainRockPaperScissors
 
-win, bot_options = BrainRockPaperScissors.start()
+while True:
+    win, bot_options = BrainRockPaperScissors.start()
 
-if win == 0:
-    print("There is a draw {}".format(bot_options))
-elif win == 1:
-    print("Well done. The computer chose {} and failed".format(bot_options))
-elif win == 2:
-    print("Sorry, but the computer chose {}".format(bot_options))
+    if win == -1:
+        break
+    elif win == 0:
+        print("There is a draw {}".format(bot_options))
+    elif win == 1:
+        print("Well done. The computer chose {} and failed".format(bot_options))
+    elif win == 2:
+        print("Sorry, but the computer chose {}".format(bot_options))
